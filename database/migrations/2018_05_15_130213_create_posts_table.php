9d5c0b03b2f87906a1d561bc38dba17f52ddae2a
ogaty <test@example.com>
2018-05-15 22:25:38 +0900

    :lemon:auth, migrate

---
+++ database/migrations/2018_05_15_130213_create_posts_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->default(1);
+            $table->string('slug', 191)->unique();
+            $table->string('custom_code', 191)->default('blog');
+            $table->string('title', 191);
+            $table->string('subtitle', 191);
+            $table->text('description_raw');
+            $table->text('description_html');
+            $table->text('content_raw');
+            $table->text('content_html');
+            $table->text('content2_row');
+            $table->text('content2_html');
+            $table->string('page_image', 191);
+            $table->string('meta_description', 191);
+            $table->tinyInteger('is_published')->default(0);
+            $table->string('layout', 191);
+            $table->tinyInteger('ad1')->default(0);
+            $table->tinyInteger('ad2')->default(0);
+            $table->timestamps();
+            $table->dateTime('published_at')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
